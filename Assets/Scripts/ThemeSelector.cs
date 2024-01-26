fff00f8a130d1eb7805855ff1595e98001983172
Fran Gambero <frangansal.13@gmail.com>
2024-01-26 21:14:16 +0100

    themeSelector good

--- Assets/Scripts/ThemeSelector.cs
+++ Assets/Scripts/ThemeSelector.cs
@@ -15,12 +15,14 @@ public class ThemeSelector : MonoBehaviour
 
                 mText = this.gameObject.GetComponent<TextMeshProUGUI>();
         mText.text = "prueba";
+
+        RunChange();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     [ContextMenu("Do Something")]
@@ -30,4 +32,25 @@ public class ThemeSelector : MonoBehaviour
 
         mText.text = myThemeList[Random.Range(0, max)];
     }
+
+    [ContextMenu("Do change")]
+    private void RunChange() {
+        StartCoroutine(ChangeWords(8));
+    }
+
+    IEnumerator ChangeWords(float time) {
+
+        float waitTime = 0.01f;
+
+        while(time > 0) {
+            waitTime += 0.001f;
+            time -= waitTime;
+           
+
+            ReturnTheme();
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+
 }
